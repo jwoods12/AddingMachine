2d24d6eb9772327a3e90aa8e79cb4879e66a32c3
johnwoods <johnwoods@192.168.1.6>
2020-10-04 13:39:22 -0700

    Added functionality to the methods

--- assignment02/src/cse360assignment02/AddingMachine.java
+++ assignment02/src/cse360assignment02/AddingMachine.java
@@ -7,30 +7,43 @@
 package cse360assignment02;
 
 public class AddingMachine {
-	
+	//stores total
 	private int total;
+	//stores history of addition and subtraction
+	private String history;
 	
+	// constructor
 	public AddingMachine() {
 		total = 0; // not needed - included for clarity
+		history = "";
 	}
 	
+	// returns total
 	public int getTotal() {
-		return 0;
+		return total;
 	}
 	
+	// adds value to total and updates history
 	public void add(int value) {
-		
+		total = total + value;
+		history = history + " + " + value;
 	}
 	
+	// subtracts value from total and updates history
 	public void subtract(int value) {
+		total = total - value;
+		history = history + " - " + value;
 	}
 	
+	// returns history
 	public String toString() {
-		return "";
+		return "0" + history;
 	}
 	
+	//clears total and history
 	public void clear() {
-		
+		total = 0;
+		history = "";
 	}	
 }
 
